e47e24274c76316e1a1790afbc682de7f60a89e7
Johnny <jp.sdc006@gmail.com>
2024-09-18 08:40:39 -0300

    spotify

---
+++ aula/spotify/server/db.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+
+export default async function conectaNaDb(){
+    mongoose.connect('mongodb+srv://admin:admin@cluster0.m8gkg.mongodb.net/spotify?retryWrites=true&w=majority&appName=Cluster0')
+
+    return mongoose.connection;
+}

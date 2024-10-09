fe5ce6b208da20657ad2702526ee18a69f5a2d7d
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-09 08:25:39 -0300

    spotify

--- aula/spotify/src/App.jsx
+++ aula/spotify/src/App.jsx
@@ -1,10 +1,23 @@
+import { useEffect, useState } from "react"
 import Cardsidebar from "./Componentes/Cardsidebar"
 import Container from "./Componentes/Container"
 import ConteudoPrincipal from "./Componentes/ConteudoPrincipal"
 import Header from "./Componentes/Header"
 import Sidebar from "./Componentes/Sidebar"
 
+
 function App() {
+
+  const [artistas, setArtistas] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  
+  useEffect(() =>{
+    fetch('http://localhost:3000/artistas')
+    .then(res => res.json())
+    .then(data => setArtistas(data))
+    .catch(err => console.log(err))
+  }, [])
+
   return (
     <>
     <Header/>
@@ -14,7 +27,16 @@ function App() {
       <Cardsidebar/>
       <Cardsidebar/>
     </Sidebar>
-    <ConteudoPrincipal/>
+    <ConteudoPrincipal>
+    {
+      artistas.map( artista => )
+    }
+    <div className="bg-red-600 h-[200px] w-[200px] p-4">
+      <div className="bg-cyan-500 h-[50px] mb-16"></div>
+      <div className="bg-cyan-500 h-[50px]"></div>
+    </div>
+
+    </ConteudoPrincipal>
     </Container>
     </>
   )
